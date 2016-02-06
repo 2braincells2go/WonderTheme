d46c5c73147e22a671cdeb9bc61571071c6a734d
Woody <2braincells2go@users.noreply.github.com>
2016-02-06 09:19:02 -0600

    Update default-content.php

--- public_html/themes/wondertron/default-content.php
+++ public_html/themes/wondertron/default-content.php
@@ -2,4 +2,4 @@
 
 $c['jumbotron'] = "<h1>Hello, world!</h1><p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>";
 
-$c['adminEmail'] = "admin@example.com";
+$c['adminEmail'] = "adminer@example.com";
